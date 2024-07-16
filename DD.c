3411220bcf6bdade4ce5c2854407d3521d1cb1fb
DemienMedich <mr.demien@bk.ru>
2024-07-16 19:51:33 +0300

    Test

--- DD.c
+++ DD.c
@@ -1,6 +1,9 @@
-#include <stdio.h>
+#include <stdio.h> 
+int main(void){
 
-int main(void) {
-    printf("Version 0.0.1");
-    return 0;
+  printf("N\t10*N\t100*N\t1000*N\n\n");  
+  for (int i=1;i<=10;i++)
+    printf("%d %d %d %d\n",i,10*i,100*i,1000*i);
+
+  return(0);
 }
