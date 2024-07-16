be3790a9ff16d7f2df2f6d31dbdb4d4f0f347502
DemienMedich <mr.demien@bk.ru>
2024-07-16 19:53:39 +0300

    Base code

--- DD.c
+++ DD.c
@@ -1,9 +1,8 @@
 #include <stdio.h> 
-int main(void){
 
-  printf("N\t10*N\t100*N\t1000*N\n\n");  
-  for (int i=1;i<=10;i++)
-    printf("%d %d %d %d\n",i,10*i,100*i,1000*i);
+int main(void){
+    
+    printf("\tVersion 0.0.1\n");
 
   return(0);
 }
